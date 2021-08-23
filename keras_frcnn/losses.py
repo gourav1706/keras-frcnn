6c819729fe594d35cd32db5887f5f8ed1cc3300c
gourav1706 <rastogigourav95@gmail.com>
2021-08-23 22:22:25 +0530

    Update losses.py

--- keras_frcnn/losses.py
+++ keras_frcnn/losses.py
@@ -1,8 +1,7 @@
 from keras import backend as K
 from keras.objectives import categorical_crossentropy
 
-if K.common.image_dim_ordering() == 'tf':
-	import tensorflow as tf
+import tensorflow as tf
 
 lambda_rpn_regr = 1.0
 lambda_rpn_class = 1.0
